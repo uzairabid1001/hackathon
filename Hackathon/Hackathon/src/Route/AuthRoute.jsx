1f2fcc1238c0b9573aa4b451207fa90ce094ca5e
Muhammad Uzair <92589829+uzairabid1001@users.noreply.github.com>
2025-08-27 20:11:42 +0500

    Delete Hackathon directory

--- Hackathon/Hackathon/src/Route/AuthRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom';
-
-const AuthRoute = () => {
-    return localStorage.getItem('userId') ? (<Outlet />) : (<Navigate to="/login" />);
-}
-
-export default AuthRoute
--- Hackathon/Hackathon/src/Route/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Outlet, Navigate} from 'react-router-dom'
-const ProtectedRoute = () => {
-  return !localStorage.getItem('userId') ? <Outlet /> : <Navigate to="/" />
-}
-
-export default ProtectedRoute
